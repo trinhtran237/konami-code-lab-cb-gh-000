eb77d3e6ffe9fe7e5f96ad2faecb4a13020e35f8
Diep Thuy <boychaos9x@gmail.com>
2020-05-09 09:16:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,32 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+// const codes = [
+//   "ArrowUp",
+//   "ArrowUp",
+//   "ArrowDown",
+//   "ArrowDown",
+//   "ArrowLeft",
+//   "ArrowRight",
+//   "ArrowLeft",
+//   "ArrowRight",
+//   "b",
+//   "a"
+// ];
 
 function init() {
   // your code here
+  let index=0;
+  document.body.addEventListener('keydown',(event)=>{
+    const key = event.key;
+    console.log(key)
+      if (key === codes[index])
+      {
+        index++;
+
+        if (index === codes.length)
+          alert ('Hurray')
+      }
+      else
+      { index=0;
+      }
+
+  })
 }
