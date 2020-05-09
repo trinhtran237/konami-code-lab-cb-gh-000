59b82c506472d93ac4b36184d3b56a06f64cd317
Diep Thuy <boychaos9x@gmail.com>
2020-05-09 09:46:25 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,15 @@
-// const codes = [
-//   "ArrowUp",
-//   "ArrowUp",
-//   "ArrowDown",
-//   "ArrowDown",
-//   "ArrowLeft",
-//   "ArrowRight",
-//   "ArrowLeft",
-//   "ArrowRight",
-//   "b",
-//   "a"
-// ];
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
 
 function init() {
   // your code here
